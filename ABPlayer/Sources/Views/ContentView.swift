d56729196c18c6aefb6f0b6d47019412623370eb
Hugo <cythbgy@gmail.com>
2026-03-07 19:54:59 -0700

    WIP: scroll layout of `up next`

--- ABPlayer/Sources/Views/ContentView.swift
+++ ABPlayer/Sources/Views/ContentView.swift
@@ -4,8 +4,42 @@ struct ContentView: View {
   let selectedMenu: MenuItem
 
   var body: some View {
-    Text(selectedMenu.rawValue)
-      .font(.largeTitle)
+    ZStack {
+      ScrollView {
+        VStack(spacing: 0) {
+          HStack {
+            Text(selectedMenu.rawValue)
+              .font(.largeTitle)
+            Spacer()
+          }
+          .padding(.horizontal, 18)
+          
+          HStack {
+            Text("Up Next")
+              .font(.title2)
+            Spacer()
+          }
+          .padding(.horizontal, 18)
+          .padding(.top, 14)
+          .padding(.bottom, 8)
+          
+          ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 6) {
+              Color.red.frame(width: 108, height: 144)
+              Color.red.frame(width: 108, height: 144)
+              Color.red.frame(width: 108, height: 144)
+              Color.red.frame(width: 108, height: 144)
+              Color.red.frame(width: 108, height: 144)
+              Color.red.frame(width: 108, height: 144)
+              Color.red.frame(width: 108, height: 144)
+            }
+            .padding(.horizontal, 18)
+          }
+
+          Spacer()
+        }
+      }
+    }
   }
 }
 
